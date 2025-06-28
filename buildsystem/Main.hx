880142bdf9ee5463a5fd8001368aaaf754e75457
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-28 11:49:07 -0800

    updated build system

--- buildsystem/Main.hx
+++ buildsystem/Main.hx
@@ -4,5 +4,9 @@ package;
 class Main {
     public static function main() {
         Sys.println("Hello, World!");
+
+        var tsukuru = new Tsukuru();
+        tsukuru.zipOutputPath = "./template/player.sbx";
+        tsukuru.build("./player.snbproj");
     }
 }
