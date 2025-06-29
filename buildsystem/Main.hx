30310456ce25918d8c60501019002671e3f0e9ab
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-28 20:56:00 -0800

    Update Main.hx

--- buildsystem/Main.hx
+++ buildsystem/Main.hx
@@ -5,8 +5,13 @@ class Main {
     public static function main() {
         Sys.println("Hello, World!");
 
+        var currentDir = Sys.getCwd();
+        trace(currentDir);
+        if (StringTools.contains(currentDir, "\\"))
+            currentDir = StringTools.replace(currentDir, "\\", "/");
+
         var tsukuru = new Tsukuru();
-        tsukuru.zipOutputPath = "./template/player.sbx";
-        tsukuru.build("./player.snbproj");
+        tsukuru.zipOutputPath = currentDir + "template/player.sbx";
+        tsukuru.build(currentDir + "player.snbproj");
     }
 }
