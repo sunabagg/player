c5320e472205714b1e989dda3bb2885ce87dffce
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-03 12:42:36 -0800

    Added open file functionality

--- src/Player.hx
+++ src/Player.hx
@@ -5,8 +5,10 @@ import sunaba.desktop.AcceptDialog;
 import sunaba.core.Vector2i;
 import sunaba.core.Rect2i;
 import sunaba.desktop.PopupMenu;
+import sunaba.desktop.FileDialog;
 import sunaba.core.ArrayList;
 import sunaba.core.Variant;
+import sunaba.FileDialogMode;
 
 class Player extends Widget {
     override function init() {
@@ -14,22 +16,36 @@ class Player extends Widget {
         var aboutDialog : AcceptDialog = AcceptDialog.toAcceptDialog(rootElement.find("aboutDialog"));
         buildAboutDialog(aboutDialog);
         var fileMenu = PopupMenu.toPopupMenu(rootElement.find("vbox/menuBarControl/menuBar/File"));
+        fileMenu.idPressed.connect((args: ArrayList) -> {
+            var id = args.get(0).toInt();
+            trace(id);
+            if (id == 0) {
+                var fileDialog = new FileDialog();
+                fileDialog.fileMode = FileDialogMode.openFile;
+                fileDialog.useNativeDialog = true;
+                fileDialog.access = 2;
+                fileDialog.title = "Open Sunaba Game";
+                fileDialog.addFilter("*.sbx", "Sunaba Game");
+                rootElement.addChild(fileDialog);
+
+                fileDialog.fileSelected.connect((args: ArrayList) -> {
+                    var path = args.get(0).toString();
+                    fileDialog.hide();
+                    fileDialog.delete();
+                    openSbx(path);
+                });
+
+                fileDialog.popupCentered(new Vector2i(0, 0));
+            }
+        });
         var helpMenu = PopupMenu.toPopupMenu(rootElement.find("vbox/menuBarControl/menuBar/Help"));
         helpMenu.systemMenuId = 4;
         helpMenu.idPressed.connect((args: ArrayList) -> {
-            try {
-                trace(args == null);
-                var idVar : Variant = args.get(0);
-                trace(idVar == null);
-                var id : Int = idVar.toInt();
-                if (id == 0) {
-                    var scaleFactor = aboutDialog.contentScaleFactor;
-                    var scaleFactorInt = Math.round(scaleFactor);
-                    aboutDialog.popupCentered(new Vector2i(300 * scaleFactorInt, 123 * scaleFactorInt));
-                }
-            }
-            catch(e) {
-                trace(e);
+            var id = args.get(0).toInt();
+            if (id == 0) {
+                var scaleFactor = aboutDialog.contentScaleFactor;
+                var scaleFactorInt = Math.round(scaleFactor);
+                aboutDialog.popupCentered(new Vector2i(300 * scaleFactorInt, 123 * scaleFactorInt));
             }
         });
     }
@@ -39,5 +55,9 @@ class Player extends Widget {
         aboutString += "Version 0.7.0\n";
         aboutString += "(C) 2022-2025 mintkat\n";
         dialog.dialogText = aboutString;
+    }
+
+    function openSbx(path: String) {
+        trace(path);
     }
 }
