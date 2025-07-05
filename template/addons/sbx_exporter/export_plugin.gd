78efcc42ebc12bb718fcf2f874100fbfe2c81b31
Lucas C. Abbas <lucasbear9@gmail.com>
2025-07-05 02:11:31 -0800

    updated export_plugin.gd

--- template/addons/sbx_exporter/export_plugin.gd
+++ template/addons/sbx_exporter/export_plugin.gd
@@ -22,6 +22,8 @@ func _export_begin(features: PackedStringArray, is_debug: bool, path: String, fl
 			export_path = res_path + path + "/Contents/Resources/"
 			print(export_path)
 		return
+	elif get_export_platform() is EditorExportPlatformLinuxBSD:
+		return
 	var dir_access = DirAccess.open(res_path)
 	for file in dir_access.get_files():
 		if (file.ends_with(".sbx") or file.ends_with(".sblib")) or file == "mobdebug.lua":
@@ -33,9 +35,21 @@ func _export_begin(features: PackedStringArray, is_debug: bool, path: String, fl
 			var export_fileaccess = FileAccess.open(export_filepath, FileAccess.WRITE)
 			if (export_fileaccess != null):
 				export_fileaccess.store_buffer(filedata)
+	if (get_export_platform() is EditorExportPlatformWindows):
+		var lib_dir_access = DirAccess.open(res_path + "lib/Windows-AMD64/")
+		for dll in lib_dir_access.get_files():
+			if not dll.contains("sunaba"):
+				var dll_filepath = res_path + "lib/Windows-AMD64/" + dll
+				var dll_fileaccess = FileAccess.open(dll_filepath, FileAccess.READ)
+				var dll_filedata = dll_fileaccess.get_buffer(dll_fileaccess.get_length())
+				var dll_export_filepath = export_path + dll
+				print("exporting file: " + dll_filepath + " to " + dll_export_filepath)
+				var dll_export_fileaccess = FileAccess.open(dll_export_filepath, FileAccess.WRITE)
+				if (dll_export_fileaccess != null):
+					dll_export_fileaccess.store_buffer(dll_filedata)
 
 func _export_end() -> void:
-	if not get_export_platform() is EditorExportPlatformMacOS:
+	if (not get_export_platform() is EditorExportPlatformMacOS) and (not get_export_platform() is EditorExportPlatformLinuxBSD):
 		return
 	var dir_access = DirAccess.open(ProjectSettings.globalize_path("res://"))
 	for file in dir_access.get_files():
